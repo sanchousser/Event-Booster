38ed2f18542647a848c8ac394229d866ffafabe2
sanchousser <golyukaleks@gmail.com>
2025-01-16 19:26:00 +0100

    upd

--- src/js/baseMarkUp.js
+++ src/js/baseMarkUp.js
@@ -1,8 +1,11 @@
 import EventsApiService from './fetchEvents';
 import cardMarkUp from '../templates/card';
+import { renderPagination } from './pagination';
 // import { createLogger } from 'vite';
 
 const list = document.querySelector('.cards__list');
+const paginationList = document.querySelector('.pagination__list');
+
 
 const eventsApiService = new EventsApiService();
 renderEvents();
@@ -14,7 +17,21 @@ export default async function renderEvents() {
     const markUp = cardMarkUp(events);
 
     list.insertAdjacentHTML('beforeend', markUp);
+    const totalPages = data.page.totalPages;
+    renderPagination(totalPages, eventsApiService.page, onPageClick)
   } catch (error) {
     console.error(error);
   }
+}
+
+function clearEventsList() {
+  list.innerHTML = '';
+}
+
+function onPageClick(newPage) {
+  if (newPage = eventsApiService.page) return;
+  EventsApiService.page = newPage;
+
+  clearEventsList();
+  renderEvents();
 }
