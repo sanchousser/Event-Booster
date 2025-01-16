cb2e5e15d6f9030627cc1319bf63c1d5170e03ce
sanchousser <golyukaleks@gmail.com>
2025-01-16 18:21:40 +0100

    upd

---
+++ src/js/countriesListMarkUp.js
@@ -0,0 +1,10 @@
+import countries from '/js/allCountries.json'
+
+const countriesList = document.querySelector('countries__list__select');
+
+const markUp = countries.map(country => {
+    return `
+    
+    `
+})
+console.log(countries)
