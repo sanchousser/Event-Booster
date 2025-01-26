178f0c6264382f8c0493025bb6b366257da221d9
OlaVasylieva7 <olhavas74@gmail.com>
2025-01-26 22:34:06 +0100

    add modaj.js backdrop, when, where, who, prices, buy tickets link

--- src/js/baseMarkUp.js
+++ src/js/baseMarkUp.js
@@ -6,14 +6,36 @@ const list = document.querySelector('.cards__list');
 
 const eventsApiService = new EventsApiService();
 renderEvents();
+
+function handleClickEvent() {
+  const cards = document.querySelectorAll('.cards__item');
+  cards.forEach(card => {
+    card.addEventListener(
+      'click',
+      event => {
+        const cardEl = event.target; 
+        const eventId = cardEl.getAttribute("data-id");
+
+        if (eventId) {
+          onCardClick(eventId);
+        }
+      },
+      { capture: true }
+    );
+  });
+}
+
+
+
 export default async function renderEvents() {
   try {
     const data = await eventsApiService.fetchEvents();
 
-    const events = data._embedded?.events || '';
+    window.events = data._embedded?.events || '';
     const markUp = cardMarkUp(events);
 
     list.insertAdjacentHTML('beforeend', markUp);
+    handleClickEvent();
   } catch (error) {
     console.error(error);
   }
