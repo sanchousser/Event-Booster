a1c3a0d02819fb8e8f5f70221e041351577a7f55
sanchousser <golyukaleks@gmail.com>
2025-01-30 11:41:50 +0100

    upd

--- src/js/baseMarkUp.js
+++ src/js/baseMarkUp.js
@@ -10,8 +10,17 @@ export default async function renderEvents() {
   try {
     const data = await eventsApiService.fetchEvents();
 
+<<<<<<< Updated upstream
     const events = data._embedded?.events || '';
+=======
+
+    // window.events = data._embedded?.events || '';
+    const events = data._embedded?.events || [];
+>>>>>>> Stashed changes
     const markUp = cardMarkUp(events);
+
+    handleClickEvent();
+
 
     list.insertAdjacentHTML('beforeend', markUp);
   } catch (error) {
